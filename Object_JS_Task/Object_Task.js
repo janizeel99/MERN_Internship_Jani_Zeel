bbfd2a698f642313bff8d23b2272cb697a8e4b11
janizeel99 <janizil800@gmail.com>
2025-02-20 17:07:48 +0530

    Add files via upload

---
+++ Object_JS_Task/Object_Task.js
@@ -0,0 +1,93 @@
+//Object 1
+let Noise_Earbuds = {
+    image: true,
+    name:"Noise Earbuds M202",
+    price: 2000,
+    rating: 4.5,
+    color: "Black",
+    discount: 10,
+    flipkart_Assured: true,
+    bank_offer: "10% off on SBI Credit Card"
+};
+console.log(Noise_Earbuds);
+
+//Object 2
+let Iphone_16 = {
+    image: true,
+    name:"Iphone 16",
+    price: 130000,
+    rating: 4.8,
+    color: "White, Black, Blue, Red",
+    discount: 12,
+    flipkart_Assured: true,
+    bank_offer: "5% off on Kotak Dedit Card"
+};
+console.log(Iphone_16);
+
+//Object 3
+let Samsung_Led= {
+    image: true,
+    name:"Samsung Led 2024 43inch 4K",
+    price: 38000,
+    rating: 4.2,
+    color: false,
+    discount: 10,
+    flipkart_Assured: true,
+    bank_offer: "10% off on Cash Payment"
+};
+console.log(Samsung_Led);
+
+//Object 4
+let polo_Tshirt = {
+    image: true,
+    name:"Polo Tshirt",
+    price: 800,
+    rating: 4,
+    color: true,
+    discount: false,
+    flipkart_Assured: true,
+    bank_offer: "2% off on Cash Payment"
+};
+console.log(polo_Tshirt);
+
+//Object 5
+let HP_Laptop = {
+    image: false,
+    name:"HP Laptop 15s AMD Ryzen 5 Quad Core",
+    price: 65000,
+    rating: 4.3,
+    color: "Silver",
+    discount: 10,
+    flipkart_Assured: true,
+    bank_offer: "14% off on HDFC Credit Card"
+};
+console.log(HP_Laptop);
+
+//Object 6
+let cello_pen = {
+    image: false,
+    name:"Cello Butterflow Ball Pen",
+    price: 20,
+    rating: 3.5,
+    color: "Blue",
+    discount: false,
+    flipkart_Assured: false,
+    bank_offer: false
+};
+console.log(cello_pen);
+
+//Object 7
+let Wakefit_Mattress = {
+    image: true,
+    name:"Wakefit Orthopedic Memory Foam Mattress",
+    price: 8000,
+    rating: 4.7,
+    color: false,
+    discount: 25,
+    flipkart_Assured: true,
+    bank_offer: false
+};
+console.log(Wakefit_Mattress);
+
+
+
